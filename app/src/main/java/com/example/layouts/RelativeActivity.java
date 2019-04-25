2e698feb15656fd50332d8727f24e1d67170d2df
fiona <fionahanafin@gmail.com>
2019-04-25 17:49:52 +0100

    x

--- app/src/main/java/com/example/layouts/RelativeActivity.java
+++ app/src/main/java/com/example/layouts/RelativeActivity.java
@@ -5,15 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class RelativeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_relative);
     }
     public void onClick(View v){
-        Intent intent = new Intent(this,RelativeActivity.class);
+        Intent intent = new Intent(this,DialActivity.class);
         startActivity(intent);
     }
 }
