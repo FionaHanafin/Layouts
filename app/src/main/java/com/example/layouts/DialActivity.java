b65347823300512ac1066d040435abb3e1617755
fiona <fionahanafin@gmail.com>
2019-04-25 18:03:17 +0100

    Added Dial pad and Intent

--- app/src/main/java/com/example/layouts/DialActivity.java
+++ app/src/main/java/com/example/layouts/DialActivity.java
@@ -1,13 +1,27 @@
 package com.example.layouts;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class DialActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
+    }
+
+    public void Dial(View v){
+        EditText edit = findViewById(R.id.inputUsername);
+        String number = edit.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+number.trim()));
+        startActivity(intent);
     }
 }
